18c1d15719aa804f6fd7bd293dcbd6e02e6fa3d7
Mohamed-Elharmouzi <bj15yl@student.sunderland.ac.uk>
2025-12-15 23:51:41 +0100

    Added IGameLibraryService interface

---
+++ ConsoleApp1/IGameLibraryService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace GameLibraryManager
+{
+    public interface IGameLibraryService
+    {
+        void AddPlayer(Player player);
+        void AddGame(Game game);
+        void RecordGameplay(int playerId, int gameId, double hours, int newScore);
+        Player FindPlayerById(int id);
+        Player FindPlayerByUsername(string username);
+        List<PlayerGameStats> GetTopPlayersByHours();
+        List<PlayerGameStats> GetTopPlayersByHoursManual();
+
+    }
+}
+
+
